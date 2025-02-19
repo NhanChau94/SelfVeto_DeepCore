7e864d0d006325cec8aeac5800e111f9770ee271
Thien Nhan Chau <tchau@cobalt-13.icecube.wisc.edu>
2025-02-19 09:29:32 -0600

    nuVeto script for computing pasing fraction

---
+++ scripts/nuVeto/create_dagman_jobs.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+import numpy as np
+
+#############################################################################################################
+#   Create .config, .submit files
+#
+submit_dir = '/home/tchau/code/condor_submit/nuVeto/'
+logdir = '/scratch/tchau/nuVeto_log/'
+if not (os.path.exists(submit_dir)): os.makedirs(submit_dir)
+if not (os.path.exists(logdir)): os.makedirs(logdir)
+
+cfig_file = f'{submit_dir}/dagman.config'
+sub_file = f'{submit_dir}/nuVeto.submit'
+curdir=os.getcwd()
+script_file = f'{curdir}/nuVeto_oscNext.py'
+
+with open(cfig_file, 'w') as c:
+    c.write('DAGMAN_MAX_JOBS_SUBMITTED=500'+'\n')
+    c.write('DAGMAN_MAX_SUBMIT_PER_INTERVAL=20'+'\n')
+    c.write('DAGMAN_USER_LOG_SCAN_INTERVAL=10'+'\n')
+    c.close()
+
+with open(sub_file, 'w') as s:
+    s.write(f'executable = {script_file}'+'\n')
+    s.write('\n')
+    s.write('initialdir = /data/user/tchau/'+'\n')
+    s.write(f'logdir = {logdir}'+'\n')
+    s.write('output = $(logdir)$(JOBNAME).$(Cluster).out'+'\n')
+    s.write('error = $(logdir)$(JOBNAME).$(Cluster).err'+'\n')
+    s.write('log = $(logdir)$(JOBNAME).$(Cluster).log'+'\n')
+    s.write('\n')
+    s.write('notification   = never'+'\n')
+    s.write('universe       = vanilla'+'\n')
+    s.write('\n')
+    s.write('should_transfer_files = YES'+'\n')
+    s.write('request_memory = 10GB'+'\n')
+    s.write('getenv = True'+'\n')
+    s.write('\n')
+    s.write('arguments = --cos_theta $(cos_theta) --nutype $(nutype) '+'\n')
+    s.write('queue')
+    s.close()
+
+#############################################################################################################
+# create list of jobs in dagfile and the submit shell script
+
+submit = f'{submit_dir}/submit.sh'
+dag_file = f'{submit_dir}/nuVeto.dag'
+cos_theta = np.linspace(0, 1, 40)
+nutypes = ["nu_e", "nu_mu", "nu_tau", "nu_ebar", "nu_mubar", "nu_taubar"]
+
+with open(dag_file, 'w') as f:
+    for nu in nutypes:
+        for cz in cos_theta:
+            JOBNAME = f"nuVeto_{nu}{cz:.3f}"
+            f.write(f'JOB {JOBNAME}'+' nuVeto.submit \n')
+            f.write(f'VARS {JOBNAME}'
+                    +f' JOBNAME="{JOBNAME}"'
+                    +f' cos_theta="{cz}" nutype="{nu}"'+'\n')
+f.close()
+
+with open(submit, 'w') as s:
+    s.write("#!/bin/sh\n")
+    s.write("condor_submit_dag -config dagman.config {}\n".format(dag_file))
+    print("condor_submit_dag -config dagman.config {}\n".format(dag_file))
+s.close()
---
+++ scripts/nuVeto/nuVeto_oscNext.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Run with the virtual environment:/data/user/tchau/Software/.virtualenvs/DC_self_veto/
+import numpy as np
+import sys, os
+import glob
+import math
+import pickle as pkl
+import argparse
+
+
+from nuVeto.nuveto import passing
+from nuVeto.utils import Units
+import crflux.models as pm
+
+
+parser = argparse.ArgumentParser(description="using nuVeto package for computing passing fraction")
+
+
+parser.add_argument("--cos_theta", type=float, default=1., help="cosine zenith value")
+parser.add_argument("--Emin", type=float, default=1., help="minimum energy")
+parser.add_argument("--Emax", type=float, default=1000., help="maximum energy")
+parser.add_argument("--nE", type=int, default=50, help="number of energy scan logarithmally")
+parser.add_argument("--nutype", type=str, default='nu_mu', help="neutrino type: nu_(e|mu)(bar)")
+parser.add_argument("--fluxtype", type=str, default='conv', help="flux type: conv|pr|_parent_")
+
+args = parser.parse_args()
+cos_theta = args.cos_theta
+Emin = args.Emin
+Emax = args.Emax
+nE = args.nE
+nutype = args.nutype
+fluxtype = args.fluxtype
+
+energies = np.logspace( np.log10(Emin), np.log10(Emin), nE )
+kind = f"{fluxtype} {nutype}"
+
+pf = [passing(enu *Units.GeV, cos_theta, kind=kind, prpl='mudet_oscNextL7',
+             pmodel=(pm.HillasGaisser2012, 'H3a'),
+             hadr='SIBYLL2.3c', depth=2100*Units.m,
+             density=('CORSIKA', ('SouthPole','December'))) for enu in  energies]
+
+output = {"cos_theta":cos_theta, "pf": pf, "energy":energies}
+outpath = f"/data/user/tchau/Sandbox/SelfVeto_DeepCore/scripts/nuVeto/output_pf/pf_oscNext_L7_{cos_theta:.3f}_{fluxtype}_{nutype}.pkl"
+with open(outpath, "wb") as file:
+    pkl.dump(output, file)
+file.close()
